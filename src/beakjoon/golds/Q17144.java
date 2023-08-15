89b3ae6e903a719e35749fd9f7f344174a2329c8
integ <integerbin2@naver.com>
2023-08-15 23:14:16 +0900

    백준 Q17144 미세먼지 안녕

--- src/beakjoon/golds/Q17144.java
+++ src/beakjoon/golds/Q17144.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//상당히 무식한 방법으로 구현했음
 public class Q17144 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
