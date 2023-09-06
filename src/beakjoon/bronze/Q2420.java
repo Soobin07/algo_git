4605266a9e280c891b529fddd9f50c30923dbdbc
Soobin07 <45987276+Soobin07@users.noreply.github.com>
2023-09-06 19:58:06 +0900

    백준 Q2420 사파리월드
    백준 Q2420 사파리월드

---
+++ src/beakjoon/bronze/Q2420.java
@@ -0,0 +1,10 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q2420 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Math.abs(sc.nextLong()-sc.nextLong()));
+	}
+}
