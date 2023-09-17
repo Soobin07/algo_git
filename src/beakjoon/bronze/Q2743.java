6f4de259c3be82013f338de3d4b53a391e24ff06
integ <integerbin2@naver.com>
2023-09-17 21:28:52 +0900

    백준 Q2743 단어 길이 재기

---
+++ src/beakjoon/bronze/Q2743.java
@@ -0,0 +1,10 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine().length());
+	}
+}
