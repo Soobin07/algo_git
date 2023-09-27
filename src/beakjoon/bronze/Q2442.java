9a97e543359aecd238e2f7dc12ee6093d1b2c71a
integ <integerbin2@naver.com>
2023-09-27 19:28:01 +0900

    백준 Q2442 별 찍기-5

---
+++ src/beakjoon/bronze/Q2442.java
@@ -0,0 +1,19 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q2442 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			for (int j = i; j < N - 1; j++) {
+				System.out.print(' ');
+			}
+			for (int j = 0; j < i * 2 + 1; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}
