329c2f361f6a0fa444fef7c47b1aff7829f41fd6
integ <integerbin2@naver.com>
2023-11-01 22:22:29 +0900

    백준 Q4101

---
+++ src/beakjoon/bronze/Q4101.java
@@ -0,0 +1,22 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q4101 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			if (A == 0 && B == 0) {
+				break;
+			}
+			if (A > B) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+		}
+	}
+}
