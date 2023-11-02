4ba5f358a97572b8a6cb02b4047b6af44dc5afdd
integ <integerbin2@naver.com>
2023-11-02 20:09:55 +0900

    백준 Q1550

---
+++ src/beakjoon/bronze/Q1550.java
@@ -0,0 +1,11 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q1550 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(Integer.decode("0x" + str));
+	}
+}
