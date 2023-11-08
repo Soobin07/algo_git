347d59ff835a62118c530d0167ba221078e34cd0
integ <integerbin2@naver.com>
2023-11-08 21:08:26 +0900

    백준 Q2745 진법 변환

---
+++ src/beakjoon/bronze/Q2745.java
@@ -0,0 +1,28 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q2745 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int N = sc.nextInt();
+
+		/*
+		int ans = 0;
+		int now = 1;
+		int len = str.length() - 1;
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(len - i);
+			if (ch >= '0' && ch <= '9') {
+				ans += (ch - '0') * now;
+			} else {
+				ans += (10 + ch - 'A') * now;
+			}
+			now *= N;
+		}
+		System.out.println(ans);
+		*/
+		System.out.println(Integer.parseInt(str, N));
+	}
+}
