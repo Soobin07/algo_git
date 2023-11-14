169b3a3652b87fece45fec68a60fb9123d704675
integ <integerbin2@naver.com>
2023-11-14 22:35:31 +0900

    백준 Q2754 학점계산

---
+++ src/beakjoon/bronze/Q2754.java
@@ -0,0 +1,29 @@
+package beakjoon.bronze;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Q2754 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+
+		HashMap<String, Double> map = new HashMap<>();
+
+		map.put("A+", 4.3);
+		map.put("A0", 4.0);
+		map.put("A-", 3.7);
+		map.put("B+", 3.3);
+		map.put("B0", 3.0);
+		map.put("B-", 2.7);
+		map.put("C+", 2.3);
+		map.put("C0", 2.0);
+		map.put("C-", 1.7);
+		map.put("D+", 1.3);
+		map.put("D0", 1.0);
+		map.put("D-", 0.7);
+		map.put("F", 0.0);
+
+		System.out.println(map.get(str));
+	}
+}
