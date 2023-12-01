c493b8235de8feb4620a4b9ec56f4a23d506a084
integ <integerbin2@naver.com>
2023-12-01 23:06:22 +0900

    백준 Q10101 삼각형 외우기

---
+++ src/beakjoon/bronze/Q10101.java
@@ -0,0 +1,24 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q10101 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if(A == 60 && B == 60 && C == 60) {
+			System.out.println("Equilateral");
+		}else if(A + B + C == 180) {
+			if(A == B || A == C || B == C) {
+				System.out.println("Isosceles");
+			}else {
+				System.out.println("Scalene");
+			}
+		}else {
+			System.out.println("Error");
+		}
+	}
+}
