a6a98e9159da2266a381feb1aa717006f9f9be3c
integ <integerbin2@naver.com>
2023-12-06 21:23:10 +0900

    백준 Q2523 별 찍기 - 13

---
+++ src/beakjoon/bronze/Q2523.java
@@ -0,0 +1,25 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q2523 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuffer sb = new StringBuffer();
+		
+		int N = sc.nextInt();
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0 ; j <= i ; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		for(int i = N-1 ; i > 0 ; i--) {
+			for(int j = i ; j > 0 ; j--) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
