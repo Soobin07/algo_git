1c144b338211caecc56f621af25571211f939628
integ <integerbin2@naver.com>
2023-12-07 20:25:04 +0900

    백준 Q15894 수학은 체육과목 입니다

---
+++ src/beakjoon/bronze/Q15894.java
@@ -0,0 +1,14 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+
+		// 1 : 4, 2 : 8, 3 : 12... 개당 4 올라감?
+
+		System.out.println(n * 4);
+	}
+}
