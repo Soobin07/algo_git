ed685ae9aef65a973ef04ae12b2315a29c06e39e
integ <integerbin2@naver.com>
2023-12-29 23:45:41 +0900

    백준 Q2355 시그마

--- src/beakjoon/bronze/Q2355.java
+++ src/beakjoon/bronze/Q2355.java
@@ -10,7 +10,7 @@ public class Q2355 {
 		long B = sc.nextInt();
 
 		//A와 B사이의 모든 수(A, B 포함) 다 더한다
-		//범위 양 끝 숫자 평균 * 범위 안 숫자 갯수 / 2
+		//범위 양 끝 숫자 평균 * 범위 안 숫자 갯수
 		long ans = (A + B) * (Math.abs(B - A) + 1) / 2;
 
 		System.out.println(ans);
