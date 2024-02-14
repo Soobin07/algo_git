0edd05fd90774f3928f6ae444a7826f44cfda18e
integ <integerbin2@naver.com>
2024-02-14 21:11:54 +0900

    백준 Q2075 N번째 큰 수

---
+++ src/beakjoon/silver/Q2075.java
@@ -0,0 +1,34 @@
+package beakjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+//N번째 큰 수
+public class Q2075 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				pq.add(Integer.parseInt(st.nextToken()));
+			}
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			pq.poll();
+		}
+		System.out.println(pq.poll());
+	}
+}
