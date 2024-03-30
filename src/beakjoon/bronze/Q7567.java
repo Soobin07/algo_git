a8bea25d3b6312f2f4e8bd2d91c09236289bf21d
integ <integerbin2@naver.com>
2024-03-30 22:39:27 +0900

    백준 Q7567 그릇

---
+++ src/beakjoon/bronze/Q7567.java
@@ -0,0 +1,23 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q7567 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char ch = '0';
+		int height = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (ch != str.charAt(i)) {
+				height += 10;
+			} else {
+				height += 5;
+			}
+			ch = str.charAt(i);
+		}
+		
+		System.out.println(height);
+	}
+}
