a6cc5504210740f815f1c927844a28264330f19f
integ <integerbin2@naver.com>
2024-03-31 22:44:12 +0900

    백준 Q11365 !밀비 급일

---
+++ src/beakjoon/bronze/Q11365.java
@@ -0,0 +1,24 @@
+package beakjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q11365 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+		while (!str.equals("END")) {
+			StringBuffer sb = new StringBuffer(str);
+			bw.append(sb.reverse().toString()).append("\n");
+			str = br.readLine();
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
