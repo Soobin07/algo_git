d9fe3adbb8fd811dbed78ebfc08d0a2a0521e091
integ <integerbin2@naver.com>
2024-04-14 19:35:28 +0900

    백준 Q15680 연세대학교

---
+++ src/beakjoon/bronze/Q15680.java
@@ -0,0 +1,15 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+//연세대학교
+public class Q15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		if (sc.nextInt() == 0) {
+			System.out.println("YONSEI");
+		} else {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
