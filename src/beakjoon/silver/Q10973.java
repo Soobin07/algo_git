7b5bd837a7bf3cb384a561248b8188cc9f8434bd
integ <integerbin2@naver.com>
2024-04-29 18:50:03 +0900

    백준 Q10973 이전 순열

---
+++ src/beakjoon/silver/Q10973.java
@@ -0,0 +1,59 @@
+package beakjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//이전 순열
+public class Q10973 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		int N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		if (before(arr, N)) {
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < N; i++) {
+				sb.append(arr[i] + " ");
+			}
+			sb.setLength(sb.length() - 1);
+			System.out.println(sb.toString());
+		} else {
+			System.out.println(-1);
+		}
+	}
+
+	static boolean before(int[] arr, int n) {
+		int i = n - 1;
+		while (i > 0 && arr[i - 1] <= arr[i])
+			i -= 1;
+
+		if (i <= 0)
+			return false;
+
+		int j = n - 1;
+		while (arr[j] >= arr[i - 1])
+			j -= 1;
+		swap(arr, i - 1, j);
+		j = n - 1;
+		while (i < j) {
+			swap(arr, i, j);
+			i += 1;
+			j -= 1;
+		}
+		return true;
+	}
+
+	static void swap(int[] arr, int a, int b) {
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
+}
