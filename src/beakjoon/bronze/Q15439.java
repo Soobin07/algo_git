6ab309d97d6beccd9cd1ce218c7ed6d18d077213
integ <integerbin2@naver.com>
2024-05-18 22:50:29 +0900

    백준 Q15439 베라의 패션

---
+++ src/beakjoon/bronze/Q15439.java
@@ -0,0 +1,12 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+//베라의 패션
+public class Q15439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println((N - 1) * N);
+	}
+}
