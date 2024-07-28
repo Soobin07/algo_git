7654ddc71f3f608a5a37032542f8898eae0c57b9
integ <integerbin2@naver.com>
2024-07-28 20:46:19 +0900

    백준 Q11728

---
+++ src/beakjoon/silver/Q11728.java
@@ -0,0 +1,38 @@
+package beakjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q11728 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N + M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = N; i < M + N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			bw.append(arr[i] + " ");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
