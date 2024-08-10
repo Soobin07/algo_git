58bd5da2bf9701aa42e8cbea397433d49db01e2f
integ <integerbin2@naver.com>
2024-08-10 20:36:25 +0900

    백준 Q24262, Q24263

---
+++ src/beakjoon/bronze/Q24262.java
@@ -0,0 +1,15 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q24262 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		// 시간복잡도
+
+		System.out.println(1); // return 1 번
+		System.out.println(0); // 상수 return
+	}
+}
---
+++ src/beakjoon/bronze/Q24263.java
@@ -0,0 +1,14 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q24263 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		//for문의 시간복잡도 O(N)
+		System.out.println(N);	//n번 돌아간다
+		System.out.println(1);	//선형. 1차식
+	}
+}
