b08508a72d399b09b84f879bcef141b687204a4c
integ <integerbin2@naver.com>
2024-08-22 22:32:08 +0900

    백준 Q11931

---
+++ src/beakjoon/silver/Q11931.java
@@ -0,0 +1,31 @@
+package beakjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Q11931 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(arr);
+
+		for (int i = 0; i < N; i++) {
+			bw.append(arr[N - 1 - i] + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
