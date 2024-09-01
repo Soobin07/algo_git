878bf6eac71a281fb4ca4624faa3fcbff99a025f
integ <integerbin2@naver.com>
2024-09-01 17:07:50 +0900

    백준 Q25372

---
+++ src/beakjoon/bronze/Q25372.java
@@ -0,0 +1,18 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q25372 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			if (str.length() >= 6 && str.length() <= 9) {
+				System.out.println("yes");
+			} else {
+				System.out.println("no");
+			}
+		}
+	}
+}
