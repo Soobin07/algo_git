a536aba634c963645679cbe6db567f0f9e86b260
integ <integerbin2@naver.com>
2024-10-07 21:50:10 +0900

    백준 Q15740

---
+++ src/beakjoon/bronze/Q15740.java
@@ -0,0 +1,15 @@
+package beakjoon.bronze;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Q15740 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		BigInteger A = new BigInteger(sc.next());
+		BigInteger B = new BigInteger(sc.next());
+
+		System.out.println(A.add(B));
+	}
+}
