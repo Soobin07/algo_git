7e42101fb31a187a227707687cc25a7dae4461fe
Soobin07 <subin_j92@naver.com>
2024-10-08 20:51:09 +0900

    백준 Q2947

--- src/beakjoon/bronze/Q2947.java
+++ src/beakjoon/bronze/Q2947.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 나무조각
+// 나무 조각
 public class Q2947 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
