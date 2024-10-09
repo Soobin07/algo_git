15d27382c6d7323137527dce63eccbcda7ac4613
Soobin07 <subin_j92@naver.com>
2024-10-09 21:17:33 +0900

    백준 Q5524

---
+++ src/beakjoon/bronze/Q5524.java
@@ -0,0 +1,14 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q5524 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(sc.next().toLowerCase());
+		}
+	}
+}
