5b1167a4ecf84da2f9283735c592cd4a4c577a11
Soobin07 <subin_j92@naver.com>
2024-10-29 20:51:33 +0900

    백준 Q11441

---
+++ src/beakjoon/silver/Q11441.java
@@ -0,0 +1,34 @@
+package beakjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q11441 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N + 1];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+		}
+
+		int M = Integer.parseInt(br.readLine());
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int first = Integer.parseInt(st.nextToken());
+			int last = Integer.parseInt(st.nextToken());
+
+			bw.append(arr[last] - arr[first - 1] + "\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
