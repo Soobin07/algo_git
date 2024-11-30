d679619629f50399916c102cdf828ae027768384
Soobin07 <subin_j92@naver.com>
2024-11-30 22:16:01 +0900

    백준 Q2153

---
+++ src/beakjoon/bronze/Q2153.py
@@ -0,0 +1,23 @@
+# 2153 소수 단어
+# 최대 52 * 20 = 1040
+
+import math
+
+word = input()
+
+total = 0
+for w in word :
+    if w.islower() :
+        total += ord(w) - 96 # a = 97
+    else :
+        total += ord(w) - 38 # A : 65
+
+isPrime = True
+for i in range(2, int(math.sqrt(total))+1) :
+    if total % i == 0 :
+        isPrime = False
+
+if isPrime:
+    print('It is a prime word.')
+else:
+    print('It is not a prime word.')
