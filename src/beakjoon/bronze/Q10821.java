5c3ab2101f18ae9002441c44401f36edf6d5f319
Soobin07 <subin_j92@naver.com>
2025-01-25 22:48:40 +0900

    백준 Q10821

---
+++ src/beakjoon/bronze/Q10821.java
@@ -0,0 +1,11 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q10821 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] arr = sc.next().split(",");
+		System.out.println(arr.length);
+	}
+}
