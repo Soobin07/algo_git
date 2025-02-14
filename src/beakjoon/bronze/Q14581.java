e8abf3743403bad47af8d890a3bf0ba533f481da
Soobin07 <subin_j92@naver.com>
2025-02-14 23:00:42 +0900

    백준 Q14581

---
+++ src/beakjoon/bronze/Q14581.java
@@ -0,0 +1,13 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q14581 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + str + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
