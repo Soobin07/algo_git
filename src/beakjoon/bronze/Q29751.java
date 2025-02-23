5c7de8d5307406ce22103f4145403bd8a3b7600f
Soobin07 <subin_j92@naver.com>
2025-02-23 19:31:56 +0900

    백준 Q29751

---
+++ src/beakjoon/bronze/Q29751.java
@@ -0,0 +1,11 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q29751 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double wi = (sc.nextInt() * sc.nextInt()) / 2.0d;
+		System.out.printf("%.01f",wi);
+	}
+}
