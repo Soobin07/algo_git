837a9e9da78e618824bc2be2eb5621cc819044dc
Soobin07 <subin_j92@naver.com>
2025-03-21 22:10:26 +0900

    백준 Q3004

---
+++ src/beakjoon/bronze/Q3004.java
@@ -0,0 +1,12 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q3004 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		System.out.println(((3 + N) / 2) * ((2 + N) / 2));
+	}
+}
