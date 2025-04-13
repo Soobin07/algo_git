9670807d63ef9a72ab4269dc6df141f1195c88fc
Soobin07 <subin_j92@naver.com>
2025-04-13 17:50:02 +0900

    백준 Q24313

---
+++ src/beakjoon/silver/Q24313.java
@@ -0,0 +1,29 @@
+package beakjoon.silver;
+
+import java.util.Scanner;
+
+public class Q24313 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a1 = sc.nextInt();
+		int a0 = sc.nextInt();
+		int c = sc.nextInt();
+		int n0 = sc.nextInt();
+
+		if (a1 > c) {
+			System.out.println(0);
+		} else if (a1 == c) {
+			if (a0 <= 0) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
+		} else {
+			if (a1 * n0 + a0 <= c * n0) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
+		}
+	}
+}
