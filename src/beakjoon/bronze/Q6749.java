9949766e1a21e915336c23b631ae8f275a9c78fd
Soobin07 <subin_j92@naver.com>
2025-04-28 21:16:59 +0900

    백준 Q6749

---
+++ src/beakjoon/bronze/Q6749.java
@@ -0,0 +1,12 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q6749 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int Y = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(2 * M - Y);
+	}
+}
