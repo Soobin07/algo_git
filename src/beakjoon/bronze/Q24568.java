d3b7cbdeb40b5b37fd3dac2110a9e3a75a75ba6b
Soobin07 <45987276+Soobin07@users.noreply.github.com>
2025-05-01 17:35:31 +0900

    백준 Q24568

---
+++ src/beakjoon/bronze/Q24568.java
@@ -0,0 +1,15 @@
+package beakjoon.bronze;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Q24568 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        int S = sc.nextInt();
+
+        System.out.println((8 * R + 3 * S) - 28);
+    }
+}
