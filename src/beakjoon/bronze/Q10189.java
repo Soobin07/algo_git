f5eaca3797e6a32e0498187ce22b8641f72077c2
Soobin07 <subin_j92@naver.com>
2025-05-15 22:29:40 +0900

    백준 Q10189

---
+++ src/beakjoon/bronze/Q10189.java
@@ -0,0 +1,10 @@
+package beakjoon.bronze;
+
+public class Q10189 {
+	public static void main(String[] args) {
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");
+	}
+}
