249879049bad911558a26cda31585c2155bfc905
Soobin07 <subin_j92@naver.com>
2025-05-19 20:41:03 +0900

    백준 Q11282

---
+++ src/beakjoon/bronze/Q11282.java
@@ -0,0 +1,11 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q11282 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println((char) (N - 1 + '가'));
+	}
+}
