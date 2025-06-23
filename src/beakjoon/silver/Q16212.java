8c453f831c36f014912c20d0d43c2fff42987358
Soobin07 <subin_j92@naver.com>
2025-06-23 22:37:12 +0900

    백준 Q16212

---
+++ src/beakjoon/silver/Q16212.java
@@ -0,0 +1,29 @@
+package beakjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q16212 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i] + " ");
+		}
+		System.out.println(sb.toString());
+	}
+}
