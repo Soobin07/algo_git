5b88a70ec14eb0967310b2132b5ea7eaabb15e05
Soobin07 <subin_j92@naver.com>
2025-06-25 22:12:01 +0900

    백준 Q14935

---
+++ src/beakjoon/bronze/Q14935.java
@@ -0,0 +1,12 @@
+package beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class Q14935 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String x = sc.next();
+		
+		System.out.println("FA");
+	}
+}
