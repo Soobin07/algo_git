48f821bf0cd858db9c1988beb484ff2d9f607aec
Soobin07 <45987276+Soobin07@users.noreply.github.com>
2025-08-17 18:25:59 +0900

    백준 Q30087

---
+++ src/beakjoon/bronze/Q30087.py
@@ -0,0 +1,11 @@
+N = int(input())
+dicts = {"Algorithm":"204", 
+         "DataAnalysis":"207",
+         "ArtificialIntelligence":"302",
+         "CyberSecurity":"B101",
+         "Network":"303",
+         "Startup":"501",
+         "TestStrategy":"105"
+        }
+for i in range(N):
+    print(dicts[input()])
