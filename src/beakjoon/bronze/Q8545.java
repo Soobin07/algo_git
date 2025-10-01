9a9e8c6d329fa6759b0fe1f65117e8b420504014
Soobin07 <45987276+Soobin07@users.noreply.github.com>
2025-10-01 20:34:08 +0900

    Q8545

---
+++ src/beakjoon/bronze/Q8545.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Q8545 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer(sc.nextLine());
+        sb.reverse();
+        System.out.println(sb.toString());
+    }
+}
