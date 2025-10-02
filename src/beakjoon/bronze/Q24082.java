e38403f2ee381b29e65e76a15eac0855e79415aa
Soobin07 <45987276+Soobin07@users.noreply.github.com>
2025-10-02 20:33:18 +0900

    Q24082

---
+++ src/beakjoon/bronze/Q24082.java
@@ -0,0 +1,11 @@
+package src.beakjoon.bronze;
+
+import java.util.*;
+
+class Q24082 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        System.out.println(x*x*x);
+    }
+}
