c033b32cf19b3f6eba15830229983ba194caf48e
Soobin07 <45987276+Soobin07@users.noreply.github.com>
2025-10-18 17:18:53 +0900

    Q2954

---
+++ src/beakjoon/bronze/Q2954.py
@@ -0,0 +1,8 @@
+arr = input()
+arr = arr.replace("apa", "a")
+arr = arr.replace("ipi", "i")
+arr = arr.replace("epe", "e")
+arr = arr.replace("opo", "o")
+arr = arr.replace("upu", "u")
+
+print(arr)
